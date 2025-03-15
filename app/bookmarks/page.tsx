1b48a03c4a91fe363affd03a156ad695bf6fd2bb
devferx <ferkki@hotmail.com>
2025-03-15 17:28:24 -0400

    refactor bookmarks page to use async/await for data fetching and add docker-compose for PostgreSQL setup

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,22 +1,13 @@
-"use client"
-
-import { useEffect, useState } from "react"
-
 import { Heading, Text } from "@chakra-ui/react"
 
 import { Bookmark } from "@/components/bookmark"
 import { BookmarkType } from "./schema"
 
-export default function Bookmarks() {
-  const [bookmarks, setBookmarks] = useState<BookmarkType[]>([])
-
-  useEffect(() => {
-    fetch("/bookmarks/api", {
-      next: { tags: ["bookmarks"] },
-    })
-      .then((response) => response.json() as Promise<{ data: BookmarkType[] }>)
-      .then(({ data }) => setBookmarks(data))
-  }, [])
+export default async function Bookmarks() {
+  const bookmarksResponse = await fetch("http://localhost:3000/bookmarks/api")
+  const bookmarks = (await bookmarksResponse.json()) as {
+    data: BookmarkType[]
+  }
 
   return (
     <main className="mt-12">
@@ -30,7 +21,7 @@ export default function Bookmarks() {
       </header>
 
       <ul className="text-lg mt-10">
-        {bookmarks?.map((bookmark) => (
+        {bookmarks?.data.map((bookmark) => (
           <li className="border-b-2 py-4 px-6 my-2" key={bookmark.id}>
             <Bookmark {...bookmark} />
             {/* <div className="my-1 text-gray-600 text-xs ml-7">
